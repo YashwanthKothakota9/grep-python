04316b8ddf151e550ea85a290e749e1dc7adb835
YashwanthK <yashwanthkothakota@gmail.com>
2024-07-23 17:07:59 +0530

    add check for match one or more items

--- app/main.py
+++ app/main.py
@@ -4,44 +4,46 @@ import sys
 # import lark - available if you need it!
 
 def match_pattern(input_line: str, pattern: str) -> bool:
-    if not input_line and not pattern:
-        return True
     if not pattern:
         return True
     if not input_line:
-        return False
+        return len(pattern) == 1 and pattern[0] == '$'
     
     if pattern[-1] == "$":
-        return input_line.endswith(pattern[0:-1])
+        return input_line.endswith(pattern[:-1])
     elif pattern[0] == "^":
         return input_line.startswith(pattern[1:])
+    elif len(pattern) > 1 and pattern[1] == "+":
+        if pattern[0] != input_line[0]:
+            return False
+        i = 1
+        while i < len(input_line) and input_line[i] == pattern[0]:
+            i += 1
+        return match_pattern(input_line[i:], pattern[2:])
     elif pattern[0] == input_line[0]:
         return match_pattern(input_line[1:], pattern[1:])
     elif pattern[:2] == "\\d":
-        # In simpler terms, this code is doing the following:
-        # It's looking for the first digit in the input line.
-        # When it finds a digit, it checks if the rest of the input line from that digit onwards matches the rest of the pattern.
-        # If it finds a match, it returns True.
-        # If it doesn't find any digits, or if none of the digits lead to a full match, it returns False.
-        return next(
-            (
-                match_pattern(input_line[i:], pattern[2:])
-                for i in range(len(input_line))
-                if input_line[i].isdigit()
-            ),
-            False,
-        )
+        for i, char in enumerate(input_line):
+            if char.isdigit():
+                if match_pattern(input_line[i+1:], pattern[2:]):
+                    return True
+        return False
     elif pattern[:2] == "\\w":
         if input_line[0].isalnum():
             return match_pattern(input_line[1:], pattern[2:])
         else:
             return False
-    elif pattern[0] == "[" and pattern[-1] == "]":
+    elif pattern[0] == "[" and "]" in pattern:
+        end = pattern.index("]")
         if pattern[1] == "^":
-            chrs = list(pattern[2:-1])
-            return all(c not in input_line for c in chrs)
-        chrs = list(pattern[1:-1])
-        return any(c in input_line for c in chrs)
+            chrs = set(pattern[2:end])
+            if input_line[0] in chrs:
+                return False
+        else:
+            chrs = set(pattern[1:end])
+            if input_line[0] not in chrs:
+                return False
+        return match_pattern(input_line[1:], pattern[end+1:])
     else:
         return match_pattern(input_line[1:], pattern)
 
